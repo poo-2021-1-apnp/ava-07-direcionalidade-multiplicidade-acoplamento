2c0a2f380156d61a6b1fda68a9df1940275b732b
Prof. Márcio Torres <marcio.torres@riogrande.ifrs.edu.br>
2021-02-16 13:52:07 -0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+// Todo projeto possui uma classe Principal, Main, App ou nome do projeto
+class App {
+  // a classe principal App possui o método principal main()
+  // se estiver usando o VSCode com a extensão para Java
+  // clique no link _Run_ a seguir:
+  public static void main(String[] args) {
+    System.out.println("Olá mundo!");
+  }
+}
